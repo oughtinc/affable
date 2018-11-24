41584dc95cf3de0622bc621103c4eef0f5be082d
Derek Elkins <derek@ought.org>
2018-11-23 16:16:38 -0800

    Still WIP. Add substitution into expressions. Refactor. Display pattern matches in order added.

--- AutoInterpreter.hs
+++ AutoInterpreter.hs
@@ -13,15 +13,12 @@ import qualified Data.Text.IO as T -- text
 import Data.Text.Lazy.Builder ( Builder, singleton, fromText ) -- text
 import Data.Text.Lazy.Builder.Int ( decimal ) -- text
 
-import Message ( Message(..), Pointer, messageToBuilder, matchMessage )
+import Message ( Message(..), Pointer, messageToBuilder, matchMessage, expandPointers )
 import Scheduler ( Event(..), SchedulerContext(..), SchedulerFn )
 import Workspace ( WorkspaceId, Workspace(..) )
 import Util ( toText )
 
 type Value = Message
-
--- Have <loc> in Undefined <loc> be a "pointer"/key for another Exp if it exists. If it doesn't exist,
--- evaluating this will lead to the creation of a new workspace.
 
 -- TODO: Make data model and figure out how to serialize to database.
 -- NOTE: Currently only ever use LetFun f (Call f _) pattern which is essentially, unsurprisingly,
@@ -55,43 +52,35 @@ expToBuilder' fBuilder vBuilder alternativesFor = go 0
 expToBuilder :: (Name -> [(Message, Exp Name Var)]) -> Exp Name Var -> Builder
 expToBuilder = expToBuilder' nameToBuilder (\v -> singleton '$' <> decimal v)
 
-{-
-exampleExp :: (Exp Text Int, Text -> [(Message, Exp Text Int)])
-exampleExp = (LetFun "f" (Call "f" (Undefined 1)), (alts M.!))
-    where alts = M.fromList [("f", [
-                                (Structured [Text "foo ", Reference 1], Undefined 2),
-                                (Structured [Text "bar ", Reference 1], LetFun "g" (Undefined 3))]),
-                             ("g", [
-                                (Structured [Text "foo ", Reference 1], Value (Text "baz")),
-                                (Structured [Text "bar ", Reference 1], Var 1)])]
--}
-
 type VarEnv v = M.Map v Value
 type FunEnv m f = M.Map f (WorkspaceId -> Value -> m Value)
 
 -- NOTE: We could do a "parallel" evaluator that might allow multiple workspaces to be scheduled.
 evaluateExp :: (Ord f, Ord v, Monad m)
             => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, Exp f v))
+            -> (VarEnv v -> Value -> Value)
             -> Exp f v
             -> m Value
-evaluateExp match = evaluateExp' match M.empty M.empty
+evaluateExp match subst = evaluateExp' match subst M.empty M.empty
 
 evaluateExp' :: (Ord f, Ord v, Monad m)
              => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, Exp f v))
+             -> (VarEnv v -> Value -> Value)
              -> VarEnv v
              -> FunEnv m f
              -> Exp f v
              -> m Value
 {-
 evaluateExp' :: (VarEnv Var -> WorkspaceId -> Name -> Message -> IO (VarEnv Var, Exp Name Var))
+             -> (VarEnv Var -> Value -> Value)
              -> VarEnv Var
              -> FunEnv IO Name
              -> Exp Name Var
              -> IO Value
 -}
-evaluateExp' match = go
+evaluateExp' match subst = go
     where go varEnv funEnv (Var x) = return $! case M.lookup x varEnv of Just v -> v
-          go varEnv funEnv (Value v) = return v
+          go varEnv funEnv (Value v) = return $ subst varEnv v
           go varEnv funEnv (Call ws f e) = do
             v <- go varEnv funEnv e
             (case M.lookup f funEnv of Just f' -> f') ws v
@@ -109,13 +98,6 @@ nameToBuilder :: Name -> Builder
 nameToBuilder ANSWER = fromText "answer"
 nameToBuilder (LOCAL i) = singleton 'f' <> decimal i
 
-{-
-type Name = (WorkspaceId, Int)
-
-nameToBuilder :: Name -> Builder
-nameToBuilder (f, i) = singleton 'f' <> decimal f <> singleton '_' <> decimal i
--}
-
 type Var = Pointer
 
 type Exp' = Exp Name Var
@@ -125,7 +107,7 @@ type Exp' = Exp Name Var
 -- concurrency.
 makeInterpreterScheduler :: SchedulerContext extra -> IO SchedulerFn
 makeInterpreterScheduler ctxt = do
-    let answerFn = ANSWER -- (0, 0) :: Name
+    let answerFn = ANSWER
     alternativesRef <- newIORef (M.empty :: M.Map Name [(Message, Exp')]) -- TODO: Load from database.
     idRef <- newIORef (0 :: Int)
 
@@ -136,7 +118,7 @@ makeInterpreterScheduler ctxt = do
 
         debugCode = do
             altMap <- readIORef alternativesRef
-            T.putStrLn (toText (expToBuilder (\f -> maybe [] id $ M.lookup f altMap) (LetFun answerFn (Value (Text "dummy")))))
+            T.putStrLn (toText (expToBuilder (\f -> maybe [] reverse $ M.lookup f altMap) (LetFun answerFn (Value (Text "dummy")))))
 
         blockOnUser mWorkspaceId = do
             putMVar requestMVar mWorkspaceId
@@ -148,7 +130,7 @@ makeInterpreterScheduler ctxt = do
 
         match varEnv workspaceId f (Reference p) = do -- TODO: This is what is desired?
             m <- dereference ctxt p
-            match varEnv workspaceId f m
+            match varEnv workspaceId f m -- TODO: Probably want to do some renumbering. Wrap the result in a Structured?
         match varEnv workspaceId f m = do
             altsMap <- readIORef alternativesRef
             case M.lookup f altsMap of
@@ -156,15 +138,9 @@ makeInterpreterScheduler ctxt = do
                     let !mMatch = asum $ map (\(p, e) -> fmap (\bindings -> (M.union varEnv bindings, e)) $ matchMessage p m) alts
                     case mMatch of
                         Just result -> return result
-                        Nothing -> do -- TODO: Need to *create* new workspaces on pattern match failure.
-                            e <- blockOnUser (Just workspaceId)
-                            pattern <- generalize ctxt m
-                            modifyIORef' alternativesRef (M.insertWith (++) f [(pattern, e)]) -- TODO: Add to end? Technically shouldn't matter.
-                            debugCode
-                            let !(Just bindings) = matchMessage pattern m -- This shouldn't fail.
-                            let varEnv' = M.union varEnv bindings
-                            return (varEnv', e)
-                Nothing -> do -- TODO: Need to *create* new workspaces on pattern match failure.
+                        Nothing -> matchFailed
+                Nothing -> matchFailed
+            where matchFailed = do -- TODO: Need to *create* new workspaces on pattern match failure.
                     e <- blockOnUser (Just workspaceId)
                     pattern <- generalize ctxt m
                     modifyIORef' alternativesRef (M.insertWith (++) f [(pattern, e)])
@@ -212,7 +188,7 @@ makeInterpreterScheduler ctxt = do
         e <- takeMVar responseMVar
         case e of -- This is a bit hacky...
             LetFun _ (Call _ _ e) -> do
-                t <- evaluateExp' match M.empty M.empty (LetFun answerFn e)
+                t <- evaluateExp' match expandPointers M.empty M.empty (LetFun answerFn e)
                 T.putStrLn (toText (messageToBuilder t))
                 blockOnUser Nothing
                 return ()
