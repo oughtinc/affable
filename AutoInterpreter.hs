d758e9a26ec6fafb26d1ab6e9ca8098123e56eb2
Derek Elkins <derek@ought.org>
2018-11-27 00:09:39 -0800

    Create new Workspaces all the time. Refactor replyFromUser.

--- AutoInterpreter.hs
+++ AutoInterpreter.hs
@@ -15,7 +15,7 @@ import Data.Text.Lazy.Builder.Int ( decimal ) -- text
 
 import Message ( Message(..), Pointer, messageToBuilder, matchMessage, expandPointers )
 import Scheduler ( Event(..), SchedulerContext(..), SchedulerFn )
-import Workspace ( WorkspaceId, Workspace(..) )
+import Workspace ( WorkspaceId, Workspace(..), emptyWorkspace )
 import Util ( toText )
 
 type Value = Message
@@ -27,7 +27,7 @@ type Value = Message
 data Exp f v
     = Var v                         -- x
     | Value Value                   -- Structured [Text "foo", Reference 1, Reference 2]
-    | Call WorkspaceId f (Exp f v)  -- f(e)
+    | Call f (Exp f v)              -- f(e)
     | LetFun f (Exp f v)            -- let f(Structured [Text "foo", p1, p2]) = e1 -- The cases arise magically, i.e. via lookup indexed by f.
   deriving ( Read, Show )           --     f(Structured [Text "bar", p1]) = e2
                                     --     f(p1) = e3
@@ -37,7 +37,7 @@ expToBuilder' :: (f -> Builder) -> (v -> Builder) -> (f -> [(Message, Exp f v)])
 expToBuilder' fBuilder vBuilder alternativesFor = go 0
     where go indent (Var x) = vBuilder x
           go indent (Value v) = valueToBuilder v
-          go indent (Call ws f e) = fBuilder f <> singleton ':' <> decimal ws <> singleton '(' <> go 0 e <> singleton ')'
+          go indent (Call f e) = fBuilder f <> {-singleton ':' <> decimal ws <>-} singleton '(' <> go 0 e <> singleton ')'
           go indent (LetFun f body) | null alts = fromText "let "
                                                <> indentBuilder <> fromText "  " <> fBuilder f <> fromText "(_) = undefined"
                                                <> indentBuilder <> fromText "in " <> go indent body
@@ -57,40 +57,43 @@ type FunEnv m f = M.Map f (WorkspaceId -> Value -> m Value)
 
 -- NOTE: We could do a "parallel" evaluator that might allow multiple workspaces to be scheduled.
 evaluateExp :: (Ord f, Ord v, Monad m)
-            => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, Exp f v))
+            => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, WorkspaceId, Exp f v))
             -> (VarEnv v -> Value -> Value)
+            -> WorkspaceId
             -> Exp f v
             -> m Value
 evaluateExp match subst = evaluateExp' match subst M.empty M.empty
 
 evaluateExp' :: (Ord f, Ord v, Monad m)
-             => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, Exp f v))
+             => (VarEnv v -> WorkspaceId -> f -> Message -> m (VarEnv v, WorkspaceId, Exp f v))
              -> (VarEnv v -> Value -> Value)
              -> VarEnv v
              -> FunEnv m f
+             -> WorkspaceId
              -> Exp f v
              -> m Value
 {-
-evaluateExp' :: (VarEnv Var -> WorkspaceId -> Name -> Message -> IO (VarEnv Var, Exp Name Var))
+evaluateExp' :: (VarEnv Var -> WorkspaceId -> Name -> Message -> IO (VarEnv Var, WorkspaceId, Exp Name Var))
              -> (VarEnv Var -> Value -> Value)
              -> VarEnv Var
              -> FunEnv IO Name
+             -> WorkspaceId
              -> Exp Name Var
              -> IO Value
 -}
 evaluateExp' match subst = go
-    where go varEnv funEnv (Var x) = return $! case M.lookup x varEnv of Just v -> v
-          go varEnv funEnv (Value v) = return $ subst varEnv v
-          go varEnv funEnv (Call ws f e) = do
-            v <- go varEnv funEnv e
+    where go varEnv funEnv ws (Var x) = return $! case M.lookup x varEnv of Just v -> v
+          go varEnv funEnv ws (Value v) = return $ subst varEnv v
+          go varEnv funEnv ws (Call f e) = do
+            v <- go varEnv funEnv ws e
             (case M.lookup f funEnv of Just f' -> f') ws v
-          go varEnv funEnv (LetFun f body) = do
+          go varEnv funEnv ws (LetFun f body) = do
             let fEvaled ws v = do
-                    (varEnv', e) <- match varEnv ws f v
-                    -- go varEnv' funEnv e -- non-recursive let
-                    go varEnv' funEnv' e -- recursive let
+                    (varEnv', ws', e) <- match varEnv ws f v
+                    -- go varEnv' funEnv ws' e -- non-recursive let
+                    go varEnv' funEnv' ws' e -- recursive let
                 funEnv' = M.insert f fEvaled funEnv
-            go varEnv funEnv' body
+            go varEnv funEnv' ws body
 
 data Name = ANSWER | LOCAL !Int deriving ( Eq, Ord, Show )
 
@@ -112,7 +115,7 @@ makeInterpreterScheduler ctxt = do
     idRef <- newIORef (0 :: Int)
 
     requestMVar <- newEmptyMVar :: IO (MVar (Maybe WorkspaceId))
-    responseMVar <- newEmptyMVar :: IO (MVar Exp')
+    responseMVar <- newEmptyMVar :: IO (MVar Event)
 
     let genSym = atomicModifyIORef' idRef (\n -> (n+1, LOCAL n))
 
@@ -120,8 +123,8 @@ makeInterpreterScheduler ctxt = do
             altMap <- readIORef alternativesRef
             T.putStrLn (toText (expToBuilder (\f -> maybe [] reverse $ M.lookup f altMap) (LetFun answerFn (Value (Text "dummy")))))
 
-        blockOnUser mWorkspaceId = do
-            putMVar requestMVar mWorkspaceId
+        blockOnUser !mWorkspace = do
+            putMVar requestMVar mWorkspace
             takeMVar responseMVar
 
         replyFromUser e = do
@@ -132,66 +135,83 @@ makeInterpreterScheduler ctxt = do
             m <- dereference ctxt p
             match varEnv workspaceId f m -- TODO: Probably want to do some renumbering. Wrap the result in a Structured?
         match varEnv workspaceId f m = do
+            workspace <- getWorkspace ctxt workspaceId
             altsMap <- readIORef alternativesRef
             case M.lookup f altsMap of
                 Just alts -> do
-                    let !mMatch = asum $ map (\(p, e) -> fmap (\bindings -> (M.union varEnv bindings, e)) $ matchMessage p m) alts
+                    let !mMatch = asum $ map (\(p, e) -> fmap (\bindings -> (p, M.union varEnv bindings, e)) $ matchMessage p m) alts
+                    -- This is a bit hacky. If this approach is the way to go, make these patterns individual constructors.
+                    -- I'd also prefer a design that only created workspace when necessary. I envision something that executes
+                    -- the automation creating nothing if there are no pattern match failures. If there is a pattern match failure,
+                    -- this will create a new workspace that will lead to the creation (via functional updating) of new workspace
+                    -- as the change percolates back up the tree of questions.
                     case mMatch of
-                        Just result -> return result
-                        Nothing -> matchFailed
-                Nothing -> matchFailed
-            where matchFailed = do -- TODO: Need to *create* new workspaces on pattern match failure.
-                    e <- blockOnUser (Just workspaceId)
+                        Just (p, varEnv', e) -> do
+                            workspace <- case f of
+                                            ANSWER -> do
+                                                newWorkspaceId <- createWorkspace ctxt workspace p
+                                                getWorkspace ctxt newWorkspaceId
+                                            _ -> return workspace
+                            let !workspaceId = identity workspace
+                            case e of
+                                LetFun _ (Call _ (Call ANSWER (Value _))) -> do -- ask case
+                                    return (varEnv', workspaceId, e)
+                                LetFun _ (Call _ (Var ptr)) -> do -- expand case
+                                    expandPointer ctxt workspace ptr
+                                    return (varEnv', workspaceId, e)
+                                Value msg -> do -- reply case
+                                    sendAnswer ctxt workspace $! expandPointers varEnv' msg -- TODO: This right?
+                                    return (varEnv', workspaceId {- Doesn't really matter -}, e)
+                                -- Just _ -> return (varEnv', workspace, e) -- Intentionally missing this case.
+                        Nothing -> matchFailed workspace
+                Nothing -> matchFailed workspace
+            where matchFailed workspace = do
                     pattern <- generalize ctxt m
+                    workspace <- case f of
+                                    ANSWER -> do
+                                        newWorkspaceId <- createWorkspace ctxt workspace pattern
+                                        getWorkspace ctxt newWorkspaceId
+                                    _ -> return workspace
+                    let !workspaceId = identity workspace
+                    evt <- blockOnUser (Just workspaceId)
+                    e <- case evt of
+                            Create msg -> do
+                                msg <- normalize ctxt msg
+                                g <- genSym
+                                return $ LetFun g (Call g (Call answerFn (Value msg)))
+                            Expand ptr -> do
+                                expandPointer ctxt workspace ptr
+                                g <- genSym
+                                return $ LetFun g (Call g (Var ptr)) -- TODO: ptr needs to correspond to a "variable"
+                            Answer msg -> do
+                                msg <- normalize ctxt msg
+                                sendAnswer ctxt workspace msg
+                                return $ Value msg
+                            -- Send ws msg -> Intentional.
                     modifyIORef' alternativesRef (M.insertWith (++) f [(pattern, e)])
                     debugCode
                     let !(Just bindings) = matchMessage pattern m -- This shouldn't fail.
                     let varEnv' = M.union varEnv bindings
-                    return (varEnv', e)
+                    return (varEnv', workspaceId, e)
 
-        scheduler user workspace (Create msg) = do
-            msg <- normalize ctxt msg
-            msg <- generalize ctxt msg -- TODO: Do this?
-            newWorkspaceId <- createWorkspace ctxt workspace msg -- TODO: Probably don't want to create workspaces here.
-            let !workspaceId = identity workspace
-            f <- genSym
-            mNewWorkspaceId <- replyFromUser $ LetFun f (Call workspaceId f (Call newWorkspaceId answerFn (Value msg)))
-            case mNewWorkspaceId of
-                Just newWorkspaceId -> Just <$> getWorkspace ctxt newWorkspaceId
-                Nothing -> return Nothing
-
-        scheduler user workspace (Answer msg) = do
-            msg <- normalize ctxt msg
-            sendAnswer ctxt workspace msg
-            mNewWorkspaceId <- replyFromUser (Value msg)
-            case mNewWorkspaceId of
-                Just newWorkspaceId -> Just <$> getWorkspace ctxt newWorkspaceId
-                Nothing -> return Nothing
-
-        scheduler user workspace (Expand ptr) = do -- TODO: Validate that ptr refers to an in-scope pointer/"variable". Is remapping needed as well?
-            expandPointer ctxt workspace ptr
-            let !workspaceId = identity workspace
-            f <- genSym
-            mNewWorkspaceId <- replyFromUser $ LetFun f (Call workspaceId f (Var ptr)) -- TODO: ptr needs to correspond to a "variable"
-            case mNewWorkspaceId of
-                Just newWorkspaceId -> Just <$> getWorkspace ctxt newWorkspaceId
-                Nothing -> return Nothing
 
         scheduler user workspace (Send ws msg) = do
             -- TODO: Think about this and support it if it makes sense.
             -- sendMessage ctxt workspace ws msg
             putStrLn "makeInterpreterScheduler: Message sending not supported."
             Just <$> getWorkspace ctxt (identity workspace)
+        scheduler _ _ evt = do
+            mWorkspaceId <- replyFromUser evt
+            case mWorkspaceId of
+                Just workspaceId -> Just <$> getWorkspace ctxt workspaceId
+                Nothing -> return Nothing
 
     forkIO $ do
-        -- let startExp = LetFun answerFn (Call answerFn (Value (Text "What is your question?")))
-        e <- takeMVar responseMVar
-        case e of -- This is a bit hacky...
-            LetFun _ (Call _ _ e) -> do
-                t <- evaluateExp' match expandPointers M.empty M.empty (LetFun answerFn e)
-                T.putStrLn (toText (messageToBuilder t))
-                blockOnUser Nothing
-                return ()
-            _ -> return ()
+        Create msg <- takeMVar responseMVar -- TODO: Better error handling.
+        let startExp = LetFun answerFn (Call answerFn (Value msg))
+        t <- evaluateExp' match expandPointers M.empty M.empty 0 startExp
+        T.putStrLn (toText (messageToBuilder t))
+        blockOnUser Nothing
+        return ()
 
     return scheduler
