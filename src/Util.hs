cdaf1eb8b3e80127c3b15b5756bc105a0259db55
Derek Elkins <derek@ought.org>
2019-02-04 17:37:47 -0800

    Add helpers for serializing and deserializing Maps.

--- src/Util.hs
+++ src/Util.hs
@@ -1,11 +1,15 @@
-module Util ( toText, invertMap, Lock, newLock, withLock ) where
+module Util ( toText, invertMap, Lock, newLock, withLock, parseMap, mapToBuilder, parseUnsafe ) where
 import Control.Concurrent.MVar ( MVar, newMVar, newEmptyMVar, putMVar, takeMVar ) -- base
 import Control.Exception ( bracket_ ) -- base
-import Data.Text.Lazy.Builder ( Builder, toLazyText ) -- text
+import Data.List ( intersperse ) -- base
+import qualified Data.Map as M -- containers
+import qualified Data.Text as S ( Text ) -- text
+import Data.Text.Lazy.Builder ( Builder, toLazyText, singleton ) -- text
 import Data.Text.Lazy ( toStrict ) -- text
 import Data.Tuple ( swap ) -- base
-import qualified Data.Text as S ( Text ) -- text
-import qualified Data.Map as M -- containers
+import Data.Void ( Void ) -- base
+import Text.Megaparsec ( Parsec, sepBy, parse ) -- megaparsec
+import Text.Megaparsec.Char ( char ) -- megaparsec
 
 toText :: Builder -> S.Text
 toText = toStrict . toLazyText
@@ -22,3 +26,15 @@ newLock = do
 
 withLock :: Lock -> IO a -> IO a
 withLock (lock, unlock) = bracket_ lock unlock
+
+mapToBuilder :: (k -> Builder) -> (v -> Builder) ->  M.Map k v -> Builder
+mapToBuilder kBuilder vBuilder m = singleton '[' <>
+    mconcat (intersperse (singleton ',') $ map (\(k, v) -> singleton '(' <> kBuilder k <> singleton ',' <> vBuilder v <> singleton ')' ) (M.toList m))
+    <> singleton ']'
+
+parseMap :: (Ord k) => Parsec Void S.Text k -> Parsec Void S.Text v -> Parsec Void S.Text (M.Map k v)
+parseMap parseKey parseValue = fmap M.fromList (char '[' *> (parseEntry `sepBy` char ',') <* char ']')
+    where parseEntry = (,) <$> (char '(' *> parseKey) <*> (char ',' *> parseValue) <* char ')'
+
+parseUnsafe :: Parsec Void S.Text a -> S.Text -> a
+parseUnsafe p t = case parse p "" t of Right x -> x
