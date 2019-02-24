57f296569419bd8b1cf9e7621e057812465500fe
Derek Elkins <derek@ought.org>
2019-02-24 15:49:38 -0800

    Add primitives to completions.

--- src/SqliteCompletionContext.hs
+++ src/SqliteCompletionContext.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module SqliteCompletionContext ( makeSqliteCompletionContext ) where
 import Data.Int ( Int64 ) -- base
-import Database.SQLite.Simple ( Connection, Only(..), NamedParam(..), query, queryNamed ) -- sqlite-simple
+import qualified Data.Text as T -- text
+import Database.SQLite.Simple ( Connection, Only(..), NamedParam(..), query, query_, queryNamed ) -- sqlite-simple
 
 import Completions ( CompletionContext(..) )
 import Exp ( Pattern )
@@ -9,18 +10,23 @@ import Message ( generalizeMessage, stripLabel, parsePatternsUnsafe )
 import Scheduler ( SchedulerContext(..), SessionId )
 import Util ( Lock, withLock )
 
+preparePattern :: T.Text -> Pattern
+preparePattern = snd . generalizeMessage 0 . stripLabel . head . parsePatternsUnsafe
+
 makeSqliteCompletionContext :: SchedulerContext (Connection, Lock) -> IO (CompletionContext (Connection, Lock))
 makeSqliteCompletionContext ctxt = do
     let (conn, lock) = extraContent ctxt
 
+    primPatterns <- map (\(Only t) -> preparePattern (T.concat ["[", t, "]"])) <$> query_ conn "SELECT pattern FROM Primitives"
+
     return $ CompletionContext {
-                    completionsFor = completionsForSqlite lock conn,
+                    completionsFor = completionsForSqlite lock conn primPatterns,
                     schedulerContext = ctxt
                 }
 
 -- NOT CACHEABLE
-completionsForSqlite :: Lock -> Connection -> SessionId -> IO [Pattern]
-completionsForSqlite lock conn sessionId = do
+completionsForSqlite :: Lock -> Connection -> [Pattern] -> SessionId -> IO [Pattern]
+completionsForSqlite lock conn primPatterns sessionId = do
     withLock lock $ do
         [fId] <- queryNamed conn "SELECT f.id \
                                  \FROM Functions f \
@@ -31,4 +37,4 @@ completionsForSqlite lock conn sessionId = do
                                  \LIMIT 1" [":sessionId" := sessionId]
 
         alts <- query conn "SELECT pattern FROM Alternatives WHERE function = ?" (fId :: Only Int64)
-        return $ map (\(Only ps) -> snd $ generalizeMessage 0 $ stripLabel $ head $ parsePatternsUnsafe ps) alts
+        return $ primPatterns ++ map (\(Only ps) -> preparePattern ps) alts
